8fda2d7278fbfdbd1463fca18625794f23fa2ec7
yassin@siouda.com <yassin@siouda.com>
2023-09-21 15:51:28 +0200

    chore(backend): db access rules

---
+++ backend/pb_migrations/1695304254_updated_jobs.js
@@ -0,0 +1,16 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("yc6m300o97t4q5r")
+
+  collection.listRule = "@request.auth.id != \"\""
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("yc6m300o97t4q5r")
+
+  collection.listRule = null
+
+  return dao.saveCollection(collection)
+})
